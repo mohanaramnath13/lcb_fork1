7fcdf9c7dd4940f8524bf382a37e05484bf8ef9d
mohanaramnath13 <mohanajich@gmail.com>
2025-12-04 12:30:33 +0530

    FIX: Added type checking in CodeGenerationProblem.__post_init__ to handle non-string contest_date values.

--- lcb_runner/benchmarks/code_generation.py
+++ lcb_runner/benchmarks/code_generation.py
@@ -56,7 +56,11 @@ class CodeGenerationProblem:
     def __post_init__(self):
         self.platform = Platform(self.platform)
         self.difficulty = Difficulty(self.difficulty)
-        self.contest_date = datetime.fromisoformat(self.contest_date)
+	if self.contest_date is not None and isinstance(self.contest_date, str):
+        	self.contest_date = datetime.fromisoformat(self.contest_date)
+    	else:
+        	self.contest_date = None
+        #self.contest_date = datetime.fromisoformat(self.contest_date)
 
         self.public_test_cases = json.loads(self.public_test_cases)  # type: ignore
         self.public_test_cases = [Test(**t) for t in self.public_test_cases]
